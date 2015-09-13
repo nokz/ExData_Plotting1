6ff91547be35200fcffd293af70b12a8f40649b3
Aaron Elvina <aelvina@gmail.com>
2015-09-13 13:32:54 -0600

    plot 2

--- plot2.R
+++ plot2.R
@@ -1,11 +1,25 @@
 library(datasets)
+library(dplyr)
+
 houseData <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?",
-                        colClasses = c("factor", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
+                        colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
 
 # Convert Date and Time columns 
-houseData$Date <- as.Date(houseData$Date, format='%d/%m/%Y')
-houseData$Time <- strptime(houseData$Time, format='%X')
-houseData$Time <- format(houseData$Time, format='%H:%M:%S')
+#houseData$Date <- as.Date(houseData$Date, format='%d/%m/%Y')
+#houseData$Time <- strptime(houseData$Time, format='%X')
+#houseData$Time <- format(houseData$Time, format='%H:%M:%S')
+
+DateTime <- paste(usage$Date, usage$Time)
+dateTime <- strptime(format = "%Y-%m-%d %H:%M:%S")
+usage$dateTime <- dateTime
 
 # Only dates from 2-day period in February, 2007
 usage <- houseData[houseData$Date >= "2007-02-01" & houseData$Date <= "2007-02-02", ]
+
+DateTime <- paste(usage$Date, usage$Time)
+dateTime <- strptime(format = "%Y-%m-%d %H:%M:%S")
+usage$dateTime <- dateTime
+
+with(usage, plot(dateTime, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)"))
+dev.copy(png, file="plot2.png")
+dev.off()
